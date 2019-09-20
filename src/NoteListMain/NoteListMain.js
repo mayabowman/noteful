bab4869266efd22b979f9a26d08ac8d1e75c1083
Maya Bowman <mayashady1@gmail.com>
2019-09-20 16:43:10 -0400

    debugging in NoteListMain

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -19,6 +19,8 @@ class NoteListMain extends React.Component {
     const { folderId } = this.props.match.params
     const { notes=[] } = this.context
     const notesForFolder = getNotesForFolder(notes, folderId)
+    console.log(notes)
+    console.log(folderId)
     return (
       <section className='NoteListMain'>
         <ul>
