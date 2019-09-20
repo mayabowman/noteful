d49477f82b19d784e4060fcb6be74bf4e7acdb0c
Maya Bowman <mayashady1@gmail.com>
2019-09-20 12:56:40 -0400

    worked through refactoring to use context

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -2,32 +2,64 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 // import { format } from 'date-fns'
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import NotefulContext from '../NotefulContext'
+import config from '../config' 
 import './Note.css';
 
-function Note(props) {
-  return (
-    <div className='Note'>
-      <h2 className='Note__title'>
-        <Link to={`/note/${props.id}`}>
-          {props.name}
-        </Link>
-      </h2>
-      <button className='Note__delete' type='button'>
-        <FontAwesomeIcon icon='trash-alt' />
-        {' '}
-        Remove
-      </button>
-      <div className='Note__dates'>
-        <div className='Note__dates-modified'>
-          Modified
+class Note extends React.Component {
+  static defaultProps = {
+    onDeleteNote: () => {},
+  }
+  static contextType = NotefulContext;
+
+  handleClickDelete = e => {
+    e.preventDefault()
+    const noteId = this.props.id
+    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+    .then(res => {
+      if (!res.ok)
+        return res.json().then(e => Promise.reject(e))
+      return res.json()
+    })
+    .then(() => {
+      this.context.deleteNote(noteId)
+      this.props.onDeleteNote(noteId)
+    })
+    .catch(error => {
+      console.error({ error })
+    })
+  }
+  render() {
+    const { name, id, modified } = this.props
+    return (
+      <div className='Note'>
+        <h2 className='Note__title'>
+          <Link to={`/note/${id}`}>
+            {name}
+          </Link>
+        </h2>
+        <button className='Note__delete' type='button'>
+          <FontAwesomeIcon icon='trash-alt' />
           {' '}
-          <span className='Date'>
-            {new Date(props.modified).getFullYear() + '/' + (new Date(props.modified).getMonth() + 1) + '/' + new Date(props.modified).getDate()}
-          </span>
+          Remove
+        </button>
+        <div className='Note__dates'>
+          <div className='Note__dates-modified'>
+            Modified
+            {' '}
+            <span className='Date'>
+              {new Date(props.modified).getFullYear() + '/' + (new Date(props.modified).getMonth() + 1) + '/' + new Date(props.modified).getDate()}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Note;
--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -2,45 +2,47 @@ import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import CircleButton from '../CircleButton/CircleButton'
+import NotefulContext from '../NotefulContext'
 import { countNotesForFolder } from '../notes-helpers'
 import './NoteListNav.css'
 
-function NoteListNav(props) {
-  return (
-    <div className='NoteListNav'>
-      <ul className='NoteListNav__list'>
-        {props.folders.map(folder => 
-          <li key={folder.id}>
-            <NavLink
-              className='NoteListNave__folder-link'
-              to={`/folder/${folder.id}`}
-            >
-              <span className='NoteListNav__num-notes'>
-                {countNotesForFolder(props.notes, folder.id)}
-              </span>
-              {folder.name}
-            </NavLink>
-          </li>
-          )}
-      </ul>
-      <div className='NoteListNav__button-wrapper'>
-          <CircleButton
-            tag={Link}
-            to='/add-folder'
-            type='button'
-            className='NoteListNav__add-folder-button'
-          >
-            <FontAwesomeIcon icon='plus' />
-            <br />
-            Folder
-          </CircleButton>
-      </div>
-    </div>
-  )
-}
+class NoteListNav extends React.Component {
+  static contextType = NotefulContext
 
-NoteListNav.defaultProps = {
-  folders: []
+  render() {
+    const { folders=[], notes=[] } = this.context
+    return (
+      <div className='NoteListNav'>
+        <ul className='NoteListNav__list'>
+          {folders.map(folder => 
+            <li key={folder.id}>
+              <NavLink
+                className='NoteListNave__folder-link'
+                to={`/folder/${folder.id}`}
+              >
+                <span className='NoteListNav__num-notes'>
+                  {countNotesForFolder(notes, folder.id)}
+                </span>
+                {folder.name}
+              </NavLink>
+            </li>
+            )}
+        </ul>
+        <div className='NoteListNav__button-wrapper'>
+            <CircleButton
+              tag={Link}
+              to='/add-folder'
+              type='button'
+              className='NoteListNav__add-folder-button'
+            >
+              <FontAwesomeIcon icon='plus' />
+              <br />
+              Folder
+            </CircleButton>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default NoteListNav;
