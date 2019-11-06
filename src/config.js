a351323a881b398be2f3c7c9385db99fb2c64afd
Maya Bowman <mayashady1@gmail.com>
2019-11-06 17:17:44 -0500

    fixed API endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   // API_ENDPOINT: 'http://localhost:8000/api',
-  API_ENDPOINT: 'https://fierce-chamber-34952.herokuapp.com/'
+  API_ENDPOINT: 'https://fierce-chamber-34952.herokuapp.com/api'
 }
